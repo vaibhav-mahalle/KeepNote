8efb1ee222e38ff32d64b7f3cdc19de9282065ef
Vaibhav Kishor Mahalle <vaibhavmahalle95@gmail.com>
2022-06-24 10:05:39 +0530

    style: improved styling of the home page

--- src/frontend/component/Filter/Filter.jsx
+++ src/frontend/component/Filter/Filter.jsx
@@ -7,25 +7,31 @@ export const Filter = () => {
       <div>
         <h3>Filter By:</h3>
       </div>
-        <h5>Date Added:</h5>
+      <h5 className="gray-col">Labels:</h5>
       <div className="filter-pill-container">
-        <div className="filter-pill txt-xs">latest</div>
-        <div className="filter-pill txt-xs">oldest</div>
+        <div className="filter-pill gray-col txt-xs">Todo</div>
+        <div className="filter-pill gray-col txt-xs">Work</div>
+        <div className="filter-pill gray-col txt-xs">Chore</div>
+        <div className="filter-pill gray-col txt-xs">Shopping</div>
       </div>
-        <h5>Priority:</h5>
+      
+      <h5 className="gray-col">Priority:</h5>
       <div className="filter-pill-container">
-        <div className="filter-pill txt-xs">low</div>
-        <div className="filter-pill txt-xs">medium</div>
-        <div className="filter-pill txt-xs">high</div>
+        <div className="filter-pill gray-col txt-xs">low</div>
+        <div className="filter-pill gray-col txt-xs">medium</div>
+        <div className="filter-pill gray-col txt-xs">high</div>
       </div>
-        <h5>Labels:</h5>
+      
+      <h5 className="gray-col">Date Added:</h5>
       <div className="filter-pill-container">
-        <div className="filter-pill txt-xs">label 123</div>
-        <div className="filter-pill txt-xs">label 123</div>
-        <div className="filter-pill txt-xs">label 123</div>
-        <div className="filter-pill txt-xs">label 123</div>
+        <div className="filter-pill gray-col txt-xs">latest</div>
+        <div className="filter-pill gray-col txt-xs">oldest</div>
       </div>
-      <button>clear</button>
+      
+      <div className="p-1 filter-btn-container">
+        <button className="filter-pill-btn primary-note-bg txt-xs">clear</button>
+        <button className="filter-pill-btn primary-col txt-xs">cancel</button>
+      </div>
     </div>
   );
 };
--- src/frontend/pages/Home/Home.jsx
+++ src/frontend/pages/Home/Home.jsx
@@ -1,17 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Home.css";
 import { Filter, TextEditor } from "../../component";
 import { GiNotebook } from "react-icons/gi";
-
+import { MdOutlineFilterAlt, MdOutlineAddCircleOutline } from "react-icons/md";
 
 export const Home = () => {
-
+  const [filterToggle, setFilterToggle] = useState(false);
+  const [addToggle, setAddToggle] = useState(false);
   return (
     <div>
-      <Filter />
-      {false ? (<TextEditor/>):(<div>
+      <div className="btn-home-container">
+        <MdOutlineFilterAlt
+          size={50}
+          color="var(--primary-notes)"
+          className="cursorPointer"
+          onClick={() => setFilterToggle((prev) => !prev)}
+        />
+        <MdOutlineAddCircleOutline
+          size={50}
+          color="var(--primary-notes)"
+          className="cursorPointer"
+          onClick={() => setAddToggle((prev) => !prev)}
+        />
+      </div>
+      {filterToggle && <div className="home-filter-container"><Filter /></div>}
+      {addToggle && <TextEditor />}
+     { !(filterToggle || addToggle) && (<div className="home-empty-container">
         <div className="gray-light empty-notes-container">
-          <GiNotebook size={150} color={"var(--primary-light)"}/>
+          <GiNotebook size={150} color={"var(--primary-light)"} />
           <p className="txt-xlg">An empty sack cannot stand upright</p>
         </div>
       </div>)}
