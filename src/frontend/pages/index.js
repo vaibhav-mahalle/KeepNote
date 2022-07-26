cf609c711a90f7a0123292a57271180ce99fbe37
Vaibhav Kishor Mahalle <vaibhavmahalle95@gmail.com>
2022-07-26 23:27:50 +0530

    fix: minor folder structure changed

--- src/frontend/pages/index.js
+++ src/frontend/pages/index.js
@@ -1,6 +1,6 @@
 export { Login, SignUp } from "./Auth";
 
-export { PageNotFound } from "./PageNotFound/Archieve/PageNotFound";
+export {PageNotFound} from "./PageNotFound/PageNotFound";
 export { Archieve } from "./Archieve/Archieve";
 
 export { Home } from "./Home/Home";
